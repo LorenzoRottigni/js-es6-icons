c6d1360433f99bec43bc042b9d884cc74cc255c0
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-26 17:42:39 +0200

    Esercizio completo

--- javascript/main.js
+++ javascript/main.js
@@ -99,26 +99,40 @@ const icons = [
 
 const iconsContainer = document.querySelector('main .container-fluid')
 
-icons.forEach(element => {
-    const newIcon = document.createElement('i')
-    const newIconDescription = document.createElement('h4');
-    const newIconContainer = document.createElement('div')
-    const {name, prefix, type, family} = element
-    newIcon.classList.add(`${family}`, `${prefix}${name}`, `fs-1`)
-    newIconContainer.appendChild(newIcon)
-    newIconDescription.textContent = name
-    newIconContainer.appendChild(newIconDescription)
-    newIconContainer.classList.add('icon-container','rounded-3','shadow')
-    switch(type){
-        case 'animal':
-            newIconContainer.classList.add('text-danger','border-danger')
-            break
-        case 'vegetable':
-            newIconContainer.classList.add('text-secondary','border-secondary')
-            break
-        case 'user':
-            newIconContainer.classList.add('text-warning','border-warning')
-            break
-    }
-    iconsContainer.appendChild(newIconContainer)
-});
+function generateIconsGrid(iconsArray, outputContainer, filter){
+    outputContainer.textContent = ''
+    iconsArray.forEach(element => {
+        const newIcon = document.createElement('i')
+        const {name, prefix, type, family} = element
+        if(type === filter || filter === 'all'){
+            const newIconDescription = document.createElement('h4');
+            const newIconContainer = document.createElement('div')
+            newIcon.classList.add(`${family}`, `${prefix}${name}`, `fs-1`)
+            newIconContainer.appendChild(newIcon)
+            newIconDescription.textContent = name
+            newIconContainer.appendChild(newIconDescription)
+            newIconContainer.classList.add('icon-container','rounded-3','shadow')
+            switch(type){
+                case 'animal':
+                    newIconContainer.classList.add('text-danger','border-danger')
+                    break
+                case 'vegetable':
+                    newIconContainer.classList.add('text-secondary','border-secondary')
+                    break
+                case 'user':
+                    newIconContainer.classList.add('text-warning','border-warning')
+                    break
+            }
+            outputContainer.appendChild(newIconContainer)
+            }
+    });
+}
+
+generateIconsGrid(icons, iconsContainer, 'all')
+
+const iconsFilter = document.querySelector('select')
+
+iconsFilter.addEventListener('change', function (){
+    console.log(this.value)
+    generateIconsGrid(icons, iconsContainer, this.value)
+})
