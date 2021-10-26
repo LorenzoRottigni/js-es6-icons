b4ccbeb61fc3294859867f770741cbc7569be058
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-26 16:51:26 +0200

    Generato layout di base

---
+++ javascript/main.js
@@ -0,0 +1,124 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+const iconsContainer = document.querySelector('main .container-fluid')
+
+icons.forEach(element => {
+    const newIcon = document.createElement('i')
+    const newIconDescription = document.createElement('h4');
+    const newIconContainer = document.createElement('div')
+    const {name, prefix, type, family} = element
+    newIcon.classList.add(`${family}`, `${prefix}${name}`, `fs-1`)
+    newIconContainer.appendChild(newIcon)
+    newIconDescription.textContent = name
+    newIconContainer.appendChild(newIconDescription)
+    newIconContainer.classList.add('icon-container','rounded-3','shadow')
+    switch(type){
+        case 'animal':
+            newIconContainer.classList.add('text-danger','border-danger')
+            break
+        case 'vegetable':
+            newIconContainer.classList.add('text-secondary','border-secondary')
+            break
+        case 'user':
+            newIconContainer.classList.add('text-warning','border-warning')
+            break
+    }
+    iconsContainer.appendChild(newIconContainer)
+});
